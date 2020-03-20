3562f33451f874e25fa6df12c1b502796afdcd8d
lijinho <58988274+lijinho@users.noreply.github.com>
2020-03-20 16:52:17 +0800

    update request header

--- pages/scrape.py
+++ pages/scrape.py
@@ -9,9 +9,3 @@ my_url="https://oporayam.com/"
 headers = {
   "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Mobile Safari/537.36"
 }
-page_html=requests.get(my_url, headers = headers)
-# html page parser part
-page_soup = soup(page_html.content, "html.parser")
-content=page_soup.findAll('button')
-content.remove(content[0])
-print (page_soup)
