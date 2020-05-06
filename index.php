4addb399166e1026fc2fdfaa12a1a1681bd2cb02
lijinho <58988274+lijinho@users.noreply.github.com>
2020-05-06 20:49:13 +0800

    display webage

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     $opts = array(
       'http'=>array(
         'method'=>"GET",
-        'header'=>"Accept-language: en\r\n" .
-                  "Cookie: foo=bar\r\n"
+        'header'=>"user-agent:Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Mobile Safari/537.36"
       )
     );
-    $site_content=file_get_contents("https://oporayam.com/");
+    $context = stream_context_create($opts);
+    $site_content=file_get_contents("https://oporayam.com/", false, $context);
+
     echo $site_content;
-    echo '<script type="text/javascript">',
-    'var elem = document.getElementsByClassName("hamburger is-closed"); elem.remove();',
+
+    echo '<script type="text/javascript"> var elem = document.getElementsByClassName("login-btn btn btn-success btn-register-lg text-uppercase"); elem.remove();',
     '</script>'
 ?>
 
