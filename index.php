4b1ce568eaf37a6b8b0c56aa07f8f14bceed1cb1
lijinho <58988274+lijinho@users.noreply.github.com>
2020-05-07 09:24:59 +0800

    some element hide

--- index.php
+++ index.php
@@ -5,15 +5,20 @@
     $opts = array(
       'http'=>array(
         'method'=>"GET",
-        'header'=>"user-agent:Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Mobile Safari/537.36"
+        'header'=>"user-agent:Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Mobile Safari/537.36\r\n".
+                  "Content-Type: application/json; charset=utf-8\r\n"
       )
     );
     $context = stream_context_create($opts);
-    $site_content=file_get_contents("https://oporayam.com/", false, $context);
+    $site_content=file_get_contents("https://oporayam.com/", true, $context);
 
     echo $site_content;
-
-    echo '<script type="text/javascript"> var elem = document.getElementsByClassName("login-btn btn btn-success btn-register-lg text-uppercase"); elem.remove();',
-    '</script>'
 ?>
-
+    <script type="text/javascript" src="jquery.min.js"></script>
+      <script type="text/javascript">
+      $(document).ready(function(){
+        $(".btn-register-lg").hide();
+        $(".hamburger").hide();
+        $(".quick-footer").hide();
+      });
+    </script>
